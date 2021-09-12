20170dccc178c17771f6729e542bfc5c73684599
Caleb <caleb.cupul@alumnos.udg.mx>
2021-09-12 15:09:56 -0500

    Basic Services

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
+
 
 
  
@@ -18,9 +20,10 @@ export class MenuComponent implements OnInit {
   
 
 
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
+    this.dishes = this.dishService.getDishes();
   }
 
   onSelect(dish: Dish) {
